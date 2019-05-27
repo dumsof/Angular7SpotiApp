13c9208b011c236d09548452370c3a7d4334701d
darwinum <dun34@hotmail.com>
2019-05-27 15:04:33 -0500

    se agrega el navbar falta boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { from } from 'rxjs';
+
+
+/*Importar el archivo que solo contiene las rutas y un vector con la definición
+ Este esta en components/app.routes.ts, RouterModule se necesita para el vector de ruta */
+
+import { ROUTER } from './components/app.routes';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +25,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTER, { useHash: true }) /* Se agrega para poder cargar las rutas. poder usar parametro useHash: true*/
   ],
   providers: [],
   bootstrap: [AppComponent]
